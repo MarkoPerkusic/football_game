99e960045c1da45f83fc257071c2a6bf0657bcb7
Marko Perkusic <marko1perkusic@gmail.com>
2024-03-21 12:49:17 +0100

    Move Player to separate module and update values

--- display/main.py
+++ display/main.py
@@ -1,7 +1,8 @@
 import pygame
-import random
 import asyncio
 import values
+
+from player import Player
 
 # Define colors
 BLUE = values.BLUE
@@ -10,22 +11,6 @@ RED = values.RED
 # Define screen dimensions
 SCREEN_WIDTH = values.WIDTH
 SCREEN_HEIGHT = values.HEIGHT
-
-class Player:
-    def __init__(self, team_color, number, x, y):
-        self.team_color = team_color
-        self.number = number
-        self.x = x
-        self.y = y
-
-    async def move(self):
-        #while True:
-        await asyncio.sleep(0.1)  # Simulate delay
-        self.x += random.randint(-1, 1)
-        self.y += random.randint(-1, 1)
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.team_color, (self.x, self.y), 10)
 
 
 async def main():
